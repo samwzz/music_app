9f05cddc057e9b54e46f40a739e3c256e0aac1b1
Sam Wang <sam.zz.wang@gmail.com>
2017-04-20 17:58:00 -0700

    Implement edit band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -14,4 +14,19 @@ class BandsController < ApplicationController
     @band = Band.find_by(id: params[:id])
     render :edit
   end
+
+  def update
+    @band = Band.find_by(id: params[:id])
+    if @band.update(band_params)
+      redirect_to band_url(@band)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
 end
